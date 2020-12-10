efbca30becfa8ae38ef2db06eef1b63054b2a3b2
ABCOM Education <abcom-mltutorials@gmai.com>
2020-12-10 13:38:38 +0530

    Source code updated

--- warn.py
+++ warn.py
@@ -25,10 +25,9 @@ def mask_detection(real_T_vid, Net, MODEL):
         if confidence>0.5:
             box= detections[0,0,i,3:7]*np.array([w,h,w,h])
             (X1,Y1,X2,Y2)=box.astype("int")
-            
             (X1, Y1) = (max(0, X1), max(0, Y1))
             (X2, Y2) = (min(w-1, X2), min(h-1, Y2))
-            #extract ROI 
+            
             face = real_T_vid[Y1:Y2, X1:X2]
             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
             face = cv2.resize(face, (224, 224))
@@ -36,25 +35,20 @@ def mask_detection(real_T_vid, Net, MODEL):
             face = preprocess_input(face)
             faces.append(face) 
             location.append((X1,Y1,X2,Y2))
-#proceed if one or more face is detected            
+        #proceed if one or more face is detected
         if len(faces)>0:
             faces = np.array(faces, dtype="float32")
             prediction = MODEL.predict(faces, batch_size=32)
         return (location, prediction)
        
-
 proto_path = r"deploy.prototxt"
 weight_path = r"res10_300x300_ssd_iter_140000.caffemodel"
 Net = cv2.dnn.readNet(proto_path, weight_path)
 
 MODEL = load_model("mask_detector.model")
 
-
 video = cv2.VideoCapture(0)
-
 while True:
-    
-        
     _, real_T_vid = video.read()
     ht,wd = real_T_vid.shape[:2]
     real_T_vid = cv2.resize(real_T_vid, dsize=(600,ht))
@@ -63,16 +57,16 @@ while True:
     for (box, pred) in zip(location, prediction):
         (X1, Y1, X2, Y2) = box
         (mask, withoutMask) = pred
-            
+                    
         label = "Mask" if mask > .75 else "No Mask"
         color = (100, 155, 0) if label == "Mask" else (0, 100, 155)
         label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
             
-        #To define the distance from camera. Greater the value shorter the distace required to show warning.
-        if X2-X1>(125):
-            #This is a % of withoutMask in front of camera. If greater than 70% then only it will show the warning.
+        # To define the distance from camera, change the constant
+        # value 60 in the following if condition
+        if X2-X1>(60):
+            # Check if prediction for without mask is 75% and above
             if withoutMask>.75:
-                    
                 cv2.putText(real_T_vid,'Maintain Distance',(X2, Y2-5),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
                 # print("Warning")
         cv2.putText(real_T_vid, label, (X1, Y1 - 10),cv2.FONT_HERSHEY_SIMPLEX, 1, color, 3)
@@ -87,5 +81,3 @@ while True:
 cv2.destroyAllWindows()
 video.stop()
         
-            
-    
